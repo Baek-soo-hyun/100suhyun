265d98c3033f77a048509cc2dcb2457e3fcad835
100su <100su@DESKTOP-5ML91ED>
2016-11-26 17:52:05 +0900

    노트북에 이클립스 설치했다.

--- src/com/hanbit/day_11_22/CalenderUtils.java
+++ src/com/hanbit/day_11_22/CalenderUtils.java
@@ -1,4 +1,4 @@
-package com.hanbit;
+package com.hanbit.day_11_22;
 
 public class CalenderUtils {
 
--- src/com/hanbit/day_11_22/Person.java
+++ src/com/hanbit/day_11_22/Person.java
@@ -1,4 +1,4 @@
-package com.hanbit;
+package com.hanbit.day_11_22;
 
 import java.math.BigDecimal;
 
